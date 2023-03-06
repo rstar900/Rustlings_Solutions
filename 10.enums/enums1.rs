1e85f2c900914d9f9fb00fe115b4f0bf5a07370a
Rachit Garg <rachitgarg900@gmail.com>
2023-03-06 20:40:14 +0100

    Add enums1

---
+++ 10.enums/enums1.rs
@@ -0,0 +1,19 @@
+// enums1.rs
+// No hints this time! ;)
+
+
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Echo,
+    Move,
+    ChangeColor 
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
