fa23952c3218c754f6d75e164214f3e35a930159
Rachit Garg <rachitgarg900@gmail.com>
2023-04-15 21:07:34 +0200

    Added errors4.rs

---
+++ 16.error_handling/errors4.rs
@@ -0,0 +1,35 @@
+// errors4.rs
+// Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
+
+
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        // Use if or match for returning appropriate error condition
+        if value == 0 {
+            return Err(CreationError::Zero)
+        } else if value < 0 {
+            return Err(CreationError::Negative)
+        }
+        Ok(PositiveNonzeroInteger(value as u64))
+    }
+}
+
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
