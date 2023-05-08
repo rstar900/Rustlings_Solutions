5f07143dac1cf6e92a3ef1121b4ee3ac7a8b0952
Rachit Garg <rachitgarg900@gmail.com>
2023-05-09 00:31:21 +0200

    Added threads2.rs

---
+++ 23.threads/threads2.rs
@@ -0,0 +1,36 @@
+// threads2.rs
+// Execute `rustlings hint threads2` or use the `hint` watch subcommand for a hint.
+// Building on the last exercise, we want all of the threads to complete their work but this time
+// the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
+
+
+
+use std::sync::{Arc, Mutex}; // Mutex also needed to be imported
+use std::thread;
+use std::time::Duration;
+
+struct JobStatus {
+    jobs_completed: u32,
+}
+
+fn main() {
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let status_shared = Arc::clone(&status);
+        let handle = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(250));
+            // You must take an action before you update a shared value
+            let mut status_ref = status_shared.lock().unwrap();
+            status_ref.jobs_completed += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+        // Print the value of the JobStatus.jobs_completed. Did you notice anything
+        // interesting in the output? Do you have to 'join' on all the handles?
+
+        println!("jobs completed {}", status.lock().unwrap().jobs_completed);
+    }
+}
