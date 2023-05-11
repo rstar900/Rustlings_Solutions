949ab9bea9fa30b77c1c4906fbca044aec588638
Rachit Garg <rachitgarg900@gmail.com>
2023-05-11 19:57:00 +0200

    Update arc1.rs

--- 24.smart_pointers/arc1.rs
+++ 24.smart_pointers/arc1.rs
@@ -30,7 +30,7 @@ fn main() {
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = Arc::clone(&shared_numbers);// TODO
+        let child_numbers = Arc::clone(&shared_numbers);
         joinhandles.push(thread::spawn(move || {
             let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
             println!("Sum of offset {} is {}", offset, sum);
