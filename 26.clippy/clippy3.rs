4a2d4e35514b1b6239171581ba922407a081967c
Rachit Garg <rachitgarg900@gmail.com>
2023-05-12 12:19:27 +0200

    Added clippy3.rs

---
+++ 26.clippy/clippy3.rs
@@ -0,0 +1,27 @@
+// clippy3.rs
+// Here's a couple more easy Clippy fixes, so you can see its utility.
+
+
+
+#[allow(unused_variables, unused_assignments)]
+fn main() {
+    let my_option: Option<()> = None;
+    if my_option.is_none() {
+        //no need of anything here
+    }
+
+    let my_arr = &[
+        -1, -2, -3,
+        -4, -5, -6
+    ];
+    println!("My array! Here it is: {:?}", my_arr);
+
+    let my_empty_vec : Vec<i32> = vec![]; // just make an empty array
+    println!("This Vec is empty, see? {:?}", my_empty_vec);
+
+    let mut value_a = 45;
+    let mut value_b = 66;
+    // Let's swap these two!
+    std::mem::swap(&mut value_a, &mut value_b); // instead of manually swapping use std::mem::swap() like this
+    println!("value a: {}; value b: {}", value_a, value_b);
+}
